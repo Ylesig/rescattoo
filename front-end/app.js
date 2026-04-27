48275b65f5acb58f630e644e4e007354b21b3203
Ylesig <gisely.vitoria@academico.ifpb.edu.br>
2026-04-27 02:19:11 +0000

    teste

--- front-end/app.js
+++ front-end/app.js
@@ -1,32 +1,7 @@
-// CARREGAR GATOS
-async function carregarGatos(){
-    try{
-        const resposta = await fetch("https://probable-bassoon-9749jjr59rgphwvj-3000.app.github.dev/gatos");
-        const gatos = await resposta.json();
-
-        const lista = document.getElementById("lista");
-        lista.innerHTML = "";
-
-        gatos.forEach(g => {
-            lista.innerHTML += `
-            <div class="card">
-                <img src="${g.foto}">
-                <h3>${g.nome}</h3>
-                <p>${g.idade} • ${g.sexo}</p>
-                <button onclick="escolherGato(${g.id})">Adotar</button>
-            </div>
-            `;
-        });
-
-    } catch(erro){
-        alert("Erro ao carregar gatos 😢");
-    }
-}
-
-// ESCOLHER GATO
 window.escolherGato = async function(id){
+
     try{
-        const resposta = await fetch(`https://probable-bassoon-9749jjr59rgphwvj-3000.app.github.dev/gatos/${id}`);
+        const resposta = await fetch(`http://localhost:3000/gatos/${id}`);
         const gato = await resposta.json();
 
         document.getElementById("fotoGato").src = gato.foto;
@@ -38,5 +13,6 @@ window.escolherGato = async function(id){
 
     } catch(erro){
         alert("Erro ao carregar gato 😢");
+        console.error(erro);
     }
 };
